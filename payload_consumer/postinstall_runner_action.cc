67b244b0d93ecc09e0bfbc3c5cc9727ea5539620
LuK1337 <priv.luk@gmail.com>
2026-07-20 16:55:54 +0200

    update_engine: Allow mapping vABC partitions even in recovery
    Sideload fails otherwise when install plan has verity disabled.
    
    Change-Id: I3d1e5e1e025e8e514ce968e53a8d11323c81b2ba
    
    [YAAP:] make an opt in soong config

--- payload_consumer/postinstall_runner_action.cc
+++ payload_consumer/postinstall_runner_action.cc
@@ -113,8 +113,12 @@ void PostinstallRunnerAction::PerformAction() {
   // If we are switching slots, then we are required to MapAllPartitions,
   // as FinishUpdate() requires all partitions to be mapped.
   // And switching slots requires FinishUpdate() to be called first
+#ifdef MAP_VABC_IN_RECOVERY
+  if (dynamic_control->GetVirtualAbFeatureFlag().IsEnabled()) {
+#else
   if (dynamic_control->GetVirtualAbFeatureFlag().IsEnabled() &&
       !constants::kIsRecovery) {
+#endif
     if (!install_plan_.partitions.empty() ||
         install_plan_.switch_slot_on_reboot) {
       if (!dynamic_control->MapAllPartitions()) {
